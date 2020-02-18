3320eaf96b2d38e3d0a57f9f4407f94253db50df
Maestria 8 <rossanana@hotmail.com>
2020-02-18 15:21:07 -0300

    Actualizado fecha 18-02-2020. Modificacion de notificaciones y reposos segunda parte

--- app/src/main/java/com/example/peoplenews/fragment/FormReposoFragment.kt
+++ app/src/main/java/com/example/peoplenews/fragment/FormReposoFragment.kt
@@ -2,6 +2,7 @@ package com.example.peoplenews.fragment
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,9 +14,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.peoplenews.adapter.ReposoAdapter
 import com.example.peoplenews.adapter.ReposoInterface
 import com.example.peoplenews.model.ReposoModel
+import com.example.peoplenews.model.RepososResult
+import com.example.peoplenews.network.RepososApi
 import kotlinx.android.synthetic.main.fragment_form_reposo.*
-import kotlinx.android.synthetic.main.fragment_solicitud_vacaciones.*
 import py.edu.uca.peoplenews.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.math.log
 
 /**
  * A simple [Fragment] subclass.
@@ -32,6 +38,16 @@ class FormReposoFragment : Fragment(),ReposoInterface {
     private  val reposos: MutableList<ReposoModel> = mutableListOf()
     var idReposo: Int = -1
 
+    private val repososApi by lazy {
+        RepososApi.create()
+    }
+
+    companion object {
+        val value = 1
+        val motivoRep = "Enfermedad"
+        //val API_KEY = "7ac3ca0156061195fead747e09bd53a1"
+        //val CATEGORY = "popular"
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,7 +67,7 @@ class FormReposoFragment : Fragment(),ReposoInterface {
         return view
     }
 
-    override fun onStart() {
+   override fun onStart() {
         super.onStart()
         if (reposos.isEmpty()){
             reposos.add(ReposoModel(1,"estoy enferma","20 de enero"))
@@ -59,6 +75,40 @@ class FormReposoFragment : Fragment(),ReposoInterface {
             adapter.notifyItemInserted(reposos.size-1)
         }
     }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        obtenerReposos()
+    }
+
+    private fun obtenerReposos() {
+        Log.d("Text","Entro a obtener reposos")
+        val call = repososApi.getReposos()
+        call.enqueue(object : Callback<RepososResult> {
+            override fun onFailure(call: retrofit2.Call<RepososResult>, t: Throwable) {
+                //Toast.makeText(requireContext(), "Hubo un error al obtener el reposo $call", Toast.LENGTH_SHORT).show()
+                Log.d("Text","Hubo un erro al obtener reposo  ${t.localizedMessage}")
+            }
+
+
+            override fun onResponse(
+                //call: retrofit2.Call<PeliculaResult>,
+                //response: retrofit2.Response<PeliculaResult>
+                call: Call<RepososResult>,
+                response: Response<RepososResult>
+
+            ) {
+                val misRepos = response.body()
+                misRepos?.results?.let { reposos.addAll(it) }
+                adapter.notifyItemInserted(reposos.size - 1)
+                Log.d("Text","Mis reposos son ${reposos.size}")
+            }
+
+        })
+
+    }
+
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
 //
---
+++ app/src/main/java/com/example/peoplenews/model/RepososResult.kt
@@ -0,0 +1,7 @@
+package com.example.peoplenews.model
+
+
+data class RepososResult(
+    val value: Int,
+    val results: ArrayList<ReposoModel>
+)
