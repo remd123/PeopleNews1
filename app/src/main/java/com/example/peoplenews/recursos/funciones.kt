8c3bd0eadd6fc502e8264534fc90d66d9e136323
Maestria 8 <rossanana@hotmail.com>
2020-02-24 22:38:55 -0300

    diseño de notificaciones. 24-02-2020

--- app/src/main/java/com/example/peoplenews/recursos/funciones.kt
+++ app/src/main/java/com/example/peoplenews/recursos/funciones.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-val URL = "http://192.168.0.7:5050/"
+val URL = "http://192.168.0.11:5050/"
 
 fun connector() : Retrofit {
 
